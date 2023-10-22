334410a391ec5f1a31292a4b79752728c58782bc
yuzyui <925312844@qq.com>
2023-10-22 21:33:27 +0800

    使用kaptcha实现了随机生成验证码的功能，用于后续的登录验证

--- src/main/java/com/wit/xzy/community/controller/LoginController.java
+++ src/main/java/com/wit/xzy/community/controller/LoginController.java
@@ -1,11 +1,22 @@
 package com.wit.xzy.community.controller;
 
+import com.google.code.kaptcha.Producer;
 import com.wit.xzy.community.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * @Author ZongYou
@@ -13,7 +24,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class LoginController {
 
-
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+    @Autowired
+    private Producer kaptchaProducer;
 
     @GetMapping("/register")
     public String getRegister(){
@@ -24,11 +37,30 @@ public class LoginController {
     public String getLoginPage() {
         return "/site/login";
     }
-    @GetMapping("/login")
-    public String login(Model model){
-        //登录页面输入有1.账号名(username)，2.密码(password),3.验证码，4.记住我
-        //与注册相同，1,2,3都需要验证输入是否准备，先验证验证码
-        //如何实现动态验证码
-        return "/site/login";
+//    @GetMapping("/login")
+//    public String login(Model model){
+//        //登录页面输入有1.账号名(username)，2.密码(password),3.验证码，4.记住我
+//        //与注册相同，1,2,3都需要验证输入是否准备，先验证验证码
+//        //如何实现动态验证码
+//        return "/site/login";
+//    }
+
+    @GetMapping("/kaptcha")
+    public void getKaptcha(HttpServletResponse response, HttpSession session){
+        //生成验证码
+        String text = kaptchaProducer.createText();
+        BufferedImage image = kaptchaProducer.createImage(text);
+
+        //将验证码图片存入Session
+        session.setAttribute("kaptch",image);
+
+        //将验证码图片输出给浏览器
+        response.setContentType("image/png");
+        try {
+            OutputStream os = response.getOutputStream();
+            ImageIO.write(image, "png", os);
+        } catch (IOException e) {
+            logger.error("响应验证码失败:" + e.getMessage());
+        }
     }
 }
