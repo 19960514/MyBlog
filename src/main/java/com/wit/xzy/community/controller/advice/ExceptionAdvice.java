fe6f973f9152fbc3488c55aa57127dbcbc5655f3
yuzyui <925312844@qq.com>
2023-10-25 20:45:50 +0800

    实现了简单统一异常处理，任何Controller层出现的错误，都跳转至错误页面。 实现了统一记录日志利用SpringAop自定义了一个切面类，pointCut为Service下的全部方法

---
+++ src/main/java/com/wit/xzy/community/controller/advice/ExceptionAdvice.java
@@ -0,0 +1,37 @@
+package com.wit.xzy.community.controller.advice;
+
+import com.wit.xzy.community.util.Commonuitl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@ControllerAdvice(annotations = Controller.class)
+public class ExceptionAdvice {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionAdvice.class);
+
+    @ExceptionHandler({Exception.class})//用于修饰方法，在Controller出现异常后被调用
+    public void handleException(Exception e, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        logger.error("服务器发生异常: " + e.getMessage());
+        for (StackTraceElement element : e.getStackTrace()) {
+            logger.error(element.toString());
+        }
+
+        String xRequestedWith = request.getHeader("x-requested-with");
+        if ("XMLHttpRequest".equals(xRequestedWith)) {
+            response.setContentType("application/plain;charset=utf-8");
+            PrintWriter writer = response.getWriter();
+            writer.write(Commonuitl.getJSONString(1, "服务器异常!"));
+        } else {
+            response.sendRedirect(request.getContextPath() + "/error");
+        }
+    }
+
+}
